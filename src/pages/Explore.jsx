2cea31fe7e704dc0f932d60b190fb03213c5cdf9
Almog Lev <almoglev3@gmail.com>
2022-01-21 18:23:25 +0200

    Create pages + routes

---
+++ src/pages/Explore.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Explore() {
+  return <div><h1>EXplore</h1></div>
+}
+
+export default Explore;
---
+++ src/pages/ForgotPassword.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ForgotPassword() {
+  return <div><h1>ForgotPassword</h1></div>
+}
+
+export default ForgotPassword;
