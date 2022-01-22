8fc5d0e2c0110fce14c9f7a9a4191f1f3d539dab
Almog Lev <almoglev3@gmail.com>
2022-01-22 17:13:25 +0200

    Register user + save it to firestore

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
-import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+import visibilityIcon from '../assets/svg/visibilityIcon.svg';
+import { getAuth, createUserWithEmailAndPassword, updateProfile} from 'firebase/auth';
+import { db } from '../firebase.config'
+import { setDoc, doc, serverTimestamp } from 'firebase/firestore'
 
 function SignUp() {
 const [showPassword, setShowPassword] = useState(false);
@@ -22,6 +25,29 @@ const onChange = (e) => {
   }))
 }
 
+const onSubmit = async (e) => {
+  e.preventDefault()
+
+  try{
+    const auth = getAuth();
+    const  userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    const user = userCredential.user;
+    updateProfile(auth.currentUser, {
+      displayName: name,
+    });
+
+    const formDataCopy = {...formData};
+    delete formDataCopy.password;
+    formDataCopy.timestamp = serverTimestamp();
+
+    await setDoc(doc(db, 'users', user.uid), formDataCopy);
+
+    navigate('/');
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 return (
   <>
     <div className="pageContainer">
@@ -30,8 +56,8 @@ return (
       </header>
 
       <main>
-        <form>
-          <input type="text" className="nameInput" placeholder='Name' id='name' value={email} onChange={onChange} />
+        <form onSubmit={onSubmit}>
+          <input type="text" className="nameInput" placeholder='Name' id='name' value={name} onChange={onChange} />
 
           <input type="email" className="emailInput" placeholder='Email' id='email' value={email} onChange={onChange} />
 
