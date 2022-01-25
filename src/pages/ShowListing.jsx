30f51b8ac92246b9033e454b778b7230c276561c
Almog Lev <almoglev3@gmail.com>
2022-01-25 22:26:45 +0200

    Add swiper images to ShowListing.jsx

--- src/pages/ShowListing.jsx
+++ src/pages/ShowListing.jsx
@@ -5,9 +5,12 @@ import { getDoc, doc } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth';
 import { db } from '../firebase.config';
 import {ReactComponent as SpinnerSVG} from '../assets/svg/spinner.svg';
-import shareIcon from '../assets/svg/shareIcon.svg'
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
-import { popup } from 'leaflet';
+import shareIcon from '../assets/svg/shareIcon.svg';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import SwiperCore, {Navigation, Pagination, Scrollbar, A11y} from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css';
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y])
 
 
 function ShowListing() {
@@ -46,7 +49,21 @@ function ShowListing() {
         )
     }
   return <main>
-      {/* slide show */}
+      
+      <Swiper slidesPerView={1} pagination={{ clickable: true }}>
+        {listing.imageUrls.map((url, index) => (
+          <SwiperSlide key={index}>
+            <div
+              style={{
+                background: `url(${listing.imageUrls[index]}) center no-repeat`,
+                backgroundSize: 'cover',
+              }}
+              className='swiperSlideDiv'
+            ></div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      
       <div className="shareIconDiv" onClick={()=> {
           navigator.clipboard.writeText(window.location.href)
           setShareLinkCopied(true)
